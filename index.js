cbc91f672b4f5c35ae41e9b94701fc844bbab103
Sang Bae <sw2.bae@gmail.com>
2020-05-08 21:30:46 -0500

    rev02 basic structure

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs =require("fs");
+const axios = require("axios");
+const util = require("util");
+// const generateMarkdown = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-];
+function questions(){
+        return inquirer.prompt([
 
-function writeToFile(fileName, data) {
-}
 
-function init() {
+    //User Input / will be sued in generateMarkdown
+
+]); }
+
+function generateMarkdown(data) {
+
+
+
+    return `
+  # ${data.title}
+  
+  `;
+  }
+  
+ 
+  
+
+
+async function init() {
+
+    try{
+
+        const userInput = questions();
+        const markdown = generateMarkdown(userInput);
+
+        await writeFileAsync("README.md",markdown);
+        console.log("***README FILE GENERATED***");
+    }
+    catch(err){
+        console.error(err);
+    }
 
 }
 
